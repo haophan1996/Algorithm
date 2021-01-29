ba37049f2efc400f2c60cd87455406e6fea1c716
Hao Phan <71293555+haophan1996@users.noreply.github.com>
2021-01-29 15:33:47 -0500

    Update Quadratic_Equations.java

--- Quadratic_Equations.java
+++ Quadratic_Equations.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class QUADRATIC_EQUATIONS {
+public class Quadratic_Equations {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
